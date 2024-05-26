e522aafcfdc9aa7e799a624028c94f349736a72d
eduarprog <eduardoantoniosanchezbaez@gmail.com>
2024-05-26 16:43:39 -0400

    CREATE DESIGN SYSTEM - HOME PART 2.

--- system.php
+++ system.php
@@ -44,7 +44,7 @@ if (isset($_SESSION["register"])) {
   <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <a class="navbar-brand"  href="system.php">System Inventory</a>
+    <a class="navbar-brand"   href="system.php">System Inventory</a>
     <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
       <div class="offcanvas-header">
         <h5 title="Usuario conectado" class="offcanvas-title" id="offcanvasNavbarLabel"><i class="fa-solid fa-user" style="color: #63E6BE;"></i>&nbsp;</i><?php echo $nombre ?></h5>
@@ -53,18 +53,24 @@ if (isset($_SESSION["register"])) {
       <div class="offcanvas-body">
         <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link active" aria-current="page" href="#"><i class="fa-solid fa-table-cells-large" style="color: #ffd43b;"></i>&nbsp;&nbsp;Dashboard</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="clients.php">Clientes</a>
+            <a class="nav-link" href="clients.php"><i class="fa-solid fa-pen-to-square" style="color: #24f948;"></i>&nbsp;&nbsp;Facturacion</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="clients.php"><i class="fa-regular fa-address-book" style="color: #153af4;"></i>&nbsp;&nbsp;Clientes</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="clients.php"><i class="fa-solid fa-chart-simple" style="color: #f00f0f;"></i>&nbsp;&nbsp;Reportes</a>
           </li>
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Productos
+            <i class="fa-solid fa-layer-group"></i>&nbsp;Productos
             </a>
             <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><a class="dropdown-item" href="#">Compras</a></li>
+              <li><a class="dropdown-item" href="#">Salidas</a></li>
               <li>
                 <hr class="dropdown-divider">
               </li>
@@ -72,16 +78,38 @@ if (isset($_SESSION["register"])) {
             </ul>
           </li>
         </ul>
-        <li class="navbar-nav">
-        <a style="text-decoration: none;" href="logout.php" title="Salir"><i class="fa-solid fa-arrow-right-from-bracket" style="color: #e40c0c;"></i></a>
-        </li>
        <!-- <form class="d-flex mt-3" role="search">
           <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
           <button class="btn btn-outline-success" type="submit">Search</button>
         </form>-->
       </div>
+      <li class="nav justify-content-end">
+        <a style="text-decoration: none;" href="logout.php" aria-label="Close" title="Salir"><i class="fa-solid fa-arrow-right-from-bracket fa-lg" style="color: #e40c0c;"></i>&nbsp;</a>
+        </li>
     </div>
+    
   </div>
+  
 </nav>
+<div class="container-fluid" style="margin: 90px; " >
+  <ul class="nav nav-tabs " id="myTab" role="tablist">
+    <li class="nav-item" role="presentation">
+      <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home-tab-pane" type="button" role="tab" aria-controls="home-tab-pane" aria-selected="true">Facturacion normal</button>
+    </li>
+    <li class="nav-item" role="presentation">
+      <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile-tab-pane" type="button" role="tab" aria-controls="profile-tab-pane" aria-selected="false">Profile</button>
+    </li>
+  </ul>
+<div class="tab-content" id="myTabContent">
+  <div class="tab-pane fade show active" id="home-tab-pane" role="tabpanel" aria-labelledby="home-tab" tabindex="0">
+    <form>
+      <label></label>
+      <br>
+      <input>
+    </form>
+  </div>
+  <div class="tab-pane fade" id="profile-tab-pane" role="tabpanel" aria-labelledby="profile-tab" tabindex="0">...</div>
+</div>
+</div>
 </body>
 </html>
